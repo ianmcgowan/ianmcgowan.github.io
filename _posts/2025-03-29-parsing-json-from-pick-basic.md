cd0fb9ca1a6f7914b45b44c21b92c13d95b9e251
Ian McGowan <ian.mcgowan@gmail.com>
2025-03-29 16:06:08 -0700

    Install "Minimal Mistakes" theme

---
+++ _posts/2025-03-29-parsing-json-from-pick-basic.md
@@ -0,0 +1,10 @@
+---
+title: "Parsing JSON data from PICK Basic"
+categories:
+  - PICK Basic
+tags:
+  - json
+  - integration
+  - tutorial
+---
+Content here...
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Welcome to the Simbian Consulting Blog",
+        "excerpt":"Welcome to the Simbian Blog This blog is designed as a source of tips, tricks and general information for Infolease users, analysts and programmers. Please contact ian@simbian.org with any suggestions, corrections or requests. Ideas for future posts include: UCI Troubleshooting Dealing with multivalues in UATB’s Exporting InfoTrieves directly to tab...","categories": ["general"],
+        "tags": [],
+        "url": "/general/2020/01/06/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "UCI Troubleshooting",
+        "excerpt":"Introduction  The Universal Contract Interface (UCI) is an Infolease module that allows contracts to be booked into Infolease from a simple flat-file format.   Sample Format   Error conditions/logs   Reviewing Flat Data  ","categories": ["technical","UCI"],
+        "tags": [],
+        "url": "/technical/uci/2020/01/08/UCI-troubleshooting.html",
+        "teaser": null
+      },{
+        "title": "Parsing JSON data from PICK Basic",
+        "excerpt":"Content here…  ","categories": ["PICK Basic"],
+        "tags": ["json","integration","tutorial"],
+        "url": "/pick%20basic/2025/03/29/parsing-json-from-pick-basic.html",
+        "teaser": null
+      }]
